cd3036276a248b5783065ad03e80d19199697928
Greg Becker <greg@codeconcepts.com>
2016-12-27 19:24:47 -0600

    Add forgotten files..

---
+++ tests/strerror.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Greg Becker.  All rights reserved.
+ */
+#include <errno.h>
+#include <string.h>
+
+/* Override strerror() for test programs so that we can normalize
+ * error messages between platforms (e.g., FreeBSD and Linux).
+ */
+char *
+strerror(int errnum)
+{
+    static char errbuf[128];
+
+    switch (errnum) {
+    case ERANGE:
+        strcpy(errbuf, "ERANGE");
+        break;
+
+    default:
+        strerror_r(errnum, errbuf, sizeof(errbuf));
+        break;
+    }
+
+    return errbuf;
+}
